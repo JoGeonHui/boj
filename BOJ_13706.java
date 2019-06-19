dfef5100af3b2e7fc18e2b5574b53f92cdf943db
JoGeonHui <ddf2356@gmail.com>
2019-06-19 16:28:13 +0900

    Add files via upload
    runtime error

---
+++ BOJ_13706.java
@@ -0,0 +1,20 @@
+package worldhello;
+
+import java.util.Scanner;
+
+public class BOJ_13706 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		n = scan.nextInt();
+		
+		double n_sqrt = Math.sqrt(n);
+		
+		System.out.println(n_sqrt);
+	}
+
+}
